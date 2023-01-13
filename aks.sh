b11b9d5e8dbfb9548d5a17ac2b76cf1008521095
Adam Kielar <adam.kielar@netguru.com>
2023-01-13 21:23:12 +0100

    Update v3

--- aks.sh
+++ aks.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+# !/bin/bash
 set -o errexit  # exit if some command fails
 set -o nounset  # check if all variables are fill
 set -o pipefail # exit if some command in pipe fails
@@ -12,7 +12,6 @@ export resourceGroup="circleci-runner-rg"
 export aksIdentity="aks-identity"
 export aksName="aks-circleci-runner"
 export kvName="aks-$RANDOM-kv"
-export storageName="aksstorage$RANDOM"
 export workloadIdentity="workload-identity"
 
 az group create --name $resourceGroup --location eastus
@@ -45,25 +44,4 @@ export oidcUrl="$(az aks show --name $aksName --resource-group $resourceGroup --
 
 az keyvault create --name $kvName \
 --resource-group $resourceGroup
-
-az storage account create --name $storageName --resource-group circleci-runner-rg --allow-blob-public-access false --kind StorageV2 --public-network-access "Enabled" --sku "Standard_LRS"
-
-az storage container create \
---account-name  $storageName \
---name "circlecirunner"
-
-az identity create --name $workloadIdentity --resource-group $resourceGroup
-
-export clientId=$(az identity show --name $workloadIdentity --resource-group $resourceGroup --query clientId -o tsv)
-
-az keyvault set-policy --name $kvName \
---secret-permissions get \
---spn $clientId
-
-export storageId=$(az storage account show --name $storageName --resource-group $resourceGroup --query id -o tsv)
-
-az role assignment create \
---assignee $clientId \
---role 'Storage Blob Data Contributor' \
---scope $storageId
 zsh
